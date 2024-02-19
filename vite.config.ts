07512e4f97bb92c7e2d8aad50276505870f7b267
Artem Kruchinin <infamoustill@gmail.com>
2024-02-19 13:55:16 +0700

    add postcss

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import * as path from "path";
+import postcssNesting from "postcss-nesting";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +11,13 @@ export default defineConfig({
   resolve: {
     alias: {
       src: path.resolve(__dirname, "./src"),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [
+        postcssNesting
+      ],
     },
   },
 })
